e924c2ac03ffb5deb5379867148ed951b0322838
urgangur <pinjaehuang@gmail.com>
2026-08-04 18:35:07 +0800

    new CSS

--- _plugins/no_date.rb
+++ _plugins/no_date.rb
@@ -1,6 +1,5 @@
 module Jekyll
   class PostReader
-    # 覆寫以允許文章檔名沒有 YYYY-MM-DD- 前綴
     def read_posts(dir)
       read_publishable(dir, "_posts", /.*\.(markdown|md)$/)
     end
--- _posts/abycity/test-subfolder-post.md
+++ _posts/abycity/test-subfolder-post.md
@@ -1,7 +1,8 @@
 ---
 layout: post
-title: "在子資料夾下的無日期文章"
+title: "細談abycity設定與世界觀"
 date: 2026-08-04
 categories: [abycity]
 ---
-這是一篇在 `abycity` 子資料夾下的文章，檔名也沒有日期前綴！
+
+
